b6ec567ee433a87bbf47210a05ba360a7338be49
bluedream-piscines <bluedream.piscines@gmail.com>
2025-07-18 17:28:22 +0200

    Ajout sécurisé sans config.php git push -u origin main

--- avis.php
+++ avis.php
@@ -1,7 +1,10 @@
 <?php
-$apiKey = "AIzaSyBwq6XMt1n0sf8qrl1-F1JTVHNR4ndnDqI";
+require_once("config.php");
+
+$apiKey = GOOGLE_API_KEY;
 $placeId = "ChIJJWP_jU2TyRIRpSE_IB6iQ4c";
 $url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&fields=reviews&language=fr&key=$apiKey";
+
 header('Content-Type: application/json');
 echo file_get_contents($url);
 ?>
